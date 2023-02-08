7d081d308c3f59f767b8a9392788189ac5c00112
Kanade <you@example.com>
2023-02-08 23:31:59 +0000

    toLowerCase

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var BaseLanguage = 'eng';
 
 function Language(language, content) {
     try {
-        return new LangManager(language).getContent(content);
+        return new LangManager(language.toLowerCase()).getContent(content);
     } catch(err) {
         if (err) return new LangManager(BaseLanguage).getContent(content);
     }
