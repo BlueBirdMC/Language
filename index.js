62237fd74d72f21432067877623051d066bd54ea
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2023-02-19 17:38:14 +0000

    Changed Var to Let
    Changed var to let because Wax asked me too

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ function language(languageName) {
     if (!fs.readdirSync(__dirname + '/Languages').includes(`${languageName}.json`)) {
         throw `Language not found.`;
     }
-    var path = `${__dirname}/Languages/${languageName}.json`;
-    var json = JSON.parse(fs.readFileSync(path).toString("utf-8"));
+    let path = `${__dirname}/Languages/${languageName}.json`;
+    let json = JSON.parse(fs.readFileSync(path).toString("utf-8"));
     return json;
 }
 
